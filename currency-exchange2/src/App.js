ed4ee65a40f7015ee3816e5b270a320bfab13786
sherotree <wytreeshero@163.com>
2019-11-26 13:05:52 +0800

    test

---
+++ currency-exchange2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Exchange } from './Exchange'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>货币兑换</h1>
+      <Exchange />
+    </div>
+  )
+}
+
+export default App
